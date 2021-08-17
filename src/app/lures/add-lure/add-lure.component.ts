a8f364937b3a8bb0e1556f9d00e50a7ffca3b8e2
Daniel Bamba <djbamba@ksu.edu>
2021-08-17 12:23:25 -0500

    Removing input Lure. PUsing form entirely.

--- src/app/lures/add-lure/add-lure.component.ts
+++ src/app/lures/add-lure/add-lure.component.ts
@@ -1,25 +1,41 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LureService } from 'src/app/services/lure.service';
 import { Lure } from '../lure';
+import { LureType } from '../lure-type';
 
 @Component({
   selector: 'app-add-lure',
   templateUrl: './add-lure.component.html',
   styleUrls: ['./add-lure.component.css'],
 })
-export class AddLureComponent implements OnInit {
-  @Input()
-  lure: Lure = new Lure()
+export class AddLureComponent implements OnChanges {
   @Output()
-  addLureReq = new EventEmitter<NgForm>()
+  addLureReq = new EventEmitter<NgForm>();
+  @ViewChild('lureForm')
+  private _lureForm!: NgForm;
 
-  constructor(private lureService: LureService, private router:Router) {}
+  get lureTypes() : LureType[] {
+    return Lure.lureTypes();
+  }
 
-  ngOnInit(): void {}
+  constructor(private lureService: LureService, private router: Router) {}
 
-  public addALure(lureForm : NgForm): void {
-    this.addLureReq.emit(lureForm)
+  ngOnChanges(changes: SimpleChanges): void {
+    console.debug('ngOnChanges: %s',JSON.stringify(changes));
+    console.debug('AddLure onChanges: %s', JSON.stringify(changes));
+    console.debug('AddLure onChanges: %s', this._lureForm?.status);
+  }
+
+  public addALure(): void {
+    this.addLureReq.emit(this._lureForm);
   }
 }
