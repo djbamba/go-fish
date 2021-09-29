c0d93c2bd29ab3032f437f49a626a0acfecf8532
Daniel Bamba <djbamba@live.com>
2021-09-28 23:11:33 -0500

    Remove unused route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
-import { AddLureComponent } from './lures/add-lure/add-lure.component';
 import { LureComponent } from './lures/lure.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  {path: 'lures', component: LureComponent},
-  {path: 'lures/add', component: AddLureComponent},
+  { path: 'lures', component: LureComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
